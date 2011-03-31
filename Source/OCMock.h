e92b418b32e0452944cf46e02e699fcc39647f77
Ramunas Jurgilas <ramunas.jurgilas@vodafone.com>
2011-03-31 14:56:06 +0200

    Additional line was added.

--- Source/OCMock.h
+++ Source/OCMock.h
@@ -3,6 +3,7 @@
 //  Copyright (c) 2004-2008 by Mulle Kybernetik. See License file for details.
 //---------------------------------------------------------------------------------------
 
+
 #import <OCMock/OCMockObject.h>
 #import <OCMock/OCMockRecorder.h>
 #import <OCMock/OCMConstraint.h>
